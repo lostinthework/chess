4ea8d209fd230c44fd0ef3b86de34d08b97234cc
lostinthework <aidenhenrie24@gmail.com>
2026-07-15 22:57:22 -0600

    added auth unit tests

--- server/src/test/java/service/UnitTests.java
+++ server/src/test/java/service/UnitTests.java
@@ -1,25 +1,113 @@
 package service;
 
 import dataaccess.*;
+import model.AuthData;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class UnitTests {
-    static final authService authyService = new authService(new MemoryUserDAO(), new MemoryAuthDAO(), new MemoryGameDAO());
+    private InterfaceUserDAO userDAO;
+    private InterfaceAuthDAO authDAO;
+    private InterfaceGameDAO gameDAO;
+
+    private authService authyService;
+    private gameService gamesService;
+    private userService useryService;
 
     @BeforeEach
     public void setuptests() {
-        InterfaceUserDAO userDAO = new MemoryUserDAO();
-        InterfaceAuthDAO authDAO = new MemoryAuthDAO();
-        InterfaceGameDAO gameDAO = new MemoryGameDAO();
-        authService authyservice = new authService(userDAO, authDAO, gameDAO);
-        gameService gamesService = new gameService(userDAO, authDAO, gameDAO);
-        gameService useryService = new gameService(userDAO, authDAO, gameDAO);
+        userDAO = new MemoryUserDAO();
+        authDAO = new MemoryAuthDAO();
+        gameDAO = new MemoryGameDAO();
+
+        authyService = new authService(userDAO, authDAO, gameDAO);
+        gamesService = new gameService(userDAO, authDAO, gameDAO);
+        useryService = new userService(userDAO, authDAO, gameDAO);
+
     }
 
     @Test
-    public void getAuth_() {
+    @Order(1)
+    @DisplayName("Get AuthData Positive")
+    public void getAuthPositive() {
+        AuthData auth = new AuthData("username", "authtoken");
+        authDAO.addAuth(auth);
+        AuthData result = authyService.getAuth("authtoken");
+        assertNotNull(result);
+        assertEquals("username", result.getUsername());
+    }
 
+    @Test
+    @Order(2)
+    @DisplayName("Get AuthData Negative")
+    public void getAuthNegative() {
+        AuthData result = authyService.getAuth("authtoken");
+        assertNull(result);
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Delete AuthData Positive")
+    public void deleteAuthPositive() {
+        AuthData auth = new AuthData("username", "authtoken");
+        authDAO.addAuth(auth);
+        authDAO.deleteAuth(auth);
+        AuthData result = authyService.getAuth("authtoken");
+        assertNull(result);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Get AuthData Negative")
+    public void AuthNegative() {
+        AuthData auth = new AuthData("username", "authtoken");
+        authDAO.deleteAuth(auth);
+        AuthData result = authyService.getAuth("authtoken");
+        assertNull(result);
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Get AuthData Positive")
+    public void getUsernamePositive() {
+        AuthData auth = new AuthData("username", "authtoken");
+        authDAO.addAuth(auth);
+        String result = authyService.getUsername("authtoken");
+        assertNotNull(result);
+        assertEquals("username", result);
+    }
+
+    @Test
+    @Order(6)
+    @DisplayName("Get AuthData Negative")
+    public void getUsernameNegative() {
+        String result = authyService.getUsername("authtoken");
+        assertNull(result);
+    }
+
+    @Test
+    @Order(7)
+    @DisplayName("Get AuthData Positive")
+    public void clearPositive() {
+        AuthData auth = new AuthData("username", "authtoken");
+        authDAO.addAuth(auth);
+        authyService.clear();
+        AuthData result = authyService.getAuth("authtoken");
+        assertNull(result);
+    }
+
+    @Test
+    @Order(8)
+    @DisplayName("Get AuthData Negative")
+    public void clearNegative() {
+        AuthData auth = new AuthData("username", "authtoken");
+        authyService.clear();
+        AuthData result = authyService.getAuth("authtoken");
+        assertNull(result);
     }
 
 }
